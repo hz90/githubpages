31921306b363b345ef2acddef54347aa4fb1cac6
object-one <61999395+object-one@users.noreply.github.com>
2021-10-30 13:24:31 +0900

    no message

---
+++ dist/precache-manifest.691988ffac333a09529fea826033cb4a.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163f20f8d962d5ccf0c2",
+    "url": "css/about.3c4a2168.css"
+  },
+  {
+    "revision": "ff50c8a60ddf2e23b051",
+    "url": "css/app.4e322ea0.css"
+  },
+  {
+    "revision": "8188fdd6d4ef1071a64246897a14cba1",
+    "url": "img/about.8188fdd6.svg"
+  },
+  {
+    "revision": "791206d4e11634deffaff19ce9dfe158",
+    "url": "img/back.791206d4.svg"
+  },
+  {
+    "revision": "de7b2aa081c48539293ed03b24b51a3e",
+    "url": "img/back.de7b2aa0.svg"
+  },
+  {
+    "revision": "f803ea7004f6f7229f66362979f73fc4",
+    "url": "img/cancel.f803ea70.svg"
+  },
+  {
+    "revision": "ddbbf337beafb24a25ec672436bfff63",
+    "url": "img/close.ddbbf337.svg"
+  },
+  {
+    "revision": "f7e7865f295165efd9bc1a3e5377c717",
+    "url": "img/close.f7e7865f.svg"
+  },
+  {
+    "revision": "cbc0e0202ce2feb05cf07b6de6d9bafa",
+    "url": "img/default.cbc0e020.png"
+  },
+  {
+    "revision": "6972dce938e0ae1812f74f089d83aede",
+    "url": "img/del.6972dce9.svg"
+  },
+  {
+    "revision": "9134dc663a58554f15932c0f7c37c7ff",
+    "url": "img/del.9134dc66.svg"
+  },
+  {
+    "revision": "a0eaf30592206a3a299aee8085714bbb",
+    "url": "img/find.a0eaf305.svg"
+  },
+  {
+    "revision": "94715fcfca43a29c9f28f194654de2d1",
+    "url": "img/friend.94715fcf.svg"
+  },
+  {
+    "revision": "903a30e8bee5d5f7fa2309c4c47d53fe",
+    "url": "img/func.903a30e8.svg"
+  },
+  {
+    "revision": "f1f39320612253263d2ff65fad387f5c",
+    "url": "img/help.f1f39320.png"
+  },
+  {
+    "revision": "7b005168bb32aa274f13e69f9b19bb50",
+    "url": "img/history.7b005168.svg"
+  },
+  {
+    "revision": "9bf68b19cea22ed528edef104a3cfe8f",
+    "url": "img/list.9bf68b19.svg"
+  },
+  {
+    "revision": "4de15dfc905ca24c509b8a58db4f0778",
+    "url": "img/listening.4de15dfc.svg"
+  },
+  {
+    "revision": "2e4dd682236aa2930c44b7b8e2f6606a",
+    "url": "img/loading.2e4dd682.svg"
+  },
+  {
+    "revision": "82113cdb5b145103b629bdd702f5a647",
+    "url": "img/love.82113cdb.svg"
+  },
+  {
+    "revision": "a18fb1d22b73114c0c0b79620ac87821",
+    "url": "img/msg.a18fb1d2.svg"
+  },
+  {
+    "revision": "60dc6ea33a4063abc0a66335d9e3c1b3",
+    "url": "img/music.60dc6ea3.svg"
+  },
+  {
+    "revision": "90e84d8752a043670754279b13e66555",
+    "url": "img/music.90e84d87.svg"
+  },
+  {
+    "revision": "864505d5a6739886a1854b1348838270",
+    "url": "img/near.864505d5.svg"
+  },
+  {
+    "revision": "3d24fd78810964803b2966211aae0c81",
+    "url": "img/next.3d24fd78.png"
+  },
+  {
+    "revision": "4b3e6223c2a729531f65fb5cd413d4b7",
+    "url": "img/pause.4b3e6223.png"
+  },
+  {
+    "revision": "4bfce8969fdd1b339a915f1c144420d7",
+    "url": "img/pause.4bfce896.svg"
+  },
+  {
+    "revision": "2d4f5ffcb1610fbd4e4947993bdb4c03",
+    "url": "img/play.2d4f5ffc.png"
+  },
+  {
+    "revision": "3a5f8ac542d406acbaba9884f1ade0dc",
+    "url": "img/play.3a5f8ac5.svg"
+  },
+  {
+    "revision": "5d33382ed2c612c0ee9bf047a577adb6",
+    "url": "img/prev.5d33382e.png"
+  },
+  {
+    "revision": "25c1827774bafe99f0900436c7a97c51",
+    "url": "img/search.25c18277.svg"
+  },
+  {
+    "revision": "980bd389c384c1ec8e2eab3d31e17a7c",
+    "url": "img/search.980bd389.svg"
+  },
+  {
+    "revision": "d07ade3502d22a78f46a71a80a76e3f6",
+    "url": "img/shop.d07ade35.svg"
+  },
+  {
+    "revision": "fbfe4549d5340eff81c43d0975c8f679",
+    "url": "img/skin.fbfe4549.svg"
+  },
+  {
+    "revision": "30dd7c5da6015ef7a53e3db781145bcd",
+    "url": "img/skinBlack.30dd7c5d.svg"
+  },
+  {
+    "revision": "dcc64b7201f2cb4fb2e97a5933f0d0f9",
+    "url": "img/skinBlue.dcc64b72.svg"
+  },
+  {
+    "revision": "7cd297ce39eeb8bb5628dd9aa90cfaa4",
+    "url": "img/skinGreen.7cd297ce.svg"
+  },
+  {
+    "revision": "983535b28dcc3e29637930e2f87628c8",
+    "url": "img/skinRed.983535b2.svg"
+  },
+  {
+    "revision": "00c7ddf56e129a21e0701483c999d442",
+    "url": "img/vip.00c7ddf5.svg"
+  },
+  {
+    "revision": "ea4acc22feeee0f58212995b2d80f5bc",
+    "url": "index.html"
+  },
+  {
+    "revision": "163f20f8d962d5ccf0c2",
+    "url": "js/about.4947a7cc.js"
+  },
+  {
+    "revision": "ff50c8a60ddf2e23b051",
+    "url": "js/app.f43da575.js"
+  },
+  {
+    "revision": "9e2e4e23eb57ef273b68",
+    "url": "js/chunk-vendors.2cfa1731.js"
+  },
+  {
+    "revision": "3794b06d153c805b551305a7cc5a7dce",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.2d8a1af458c2d099500f369da1130db4.js"
+  "precache-manifest.691988ffac333a09529fea826033cb4a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "spa_admin"});
